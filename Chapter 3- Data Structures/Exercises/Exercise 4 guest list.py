02ff0a4e7836c87f4f864c8520eb1ff3c3b42ade
rjvisperas <113714739+rjvisperas@users.noreply.github.com>
2022-10-23 14:15:08 +0400

    Guest List

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4 guest list.py
@@ -0,0 +1,10 @@
+guests = ['Klaithem bintwalee', 'Errol Carranza', 'Dan Franco']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
