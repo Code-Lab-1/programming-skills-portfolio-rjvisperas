ebf885e951487b718f62a77d5493e1c0e34d639f
rjvisperas <113714739+rjvisperas@users.noreply.github.com>
2022-11-25 22:08:54 +0400

    Add files via upload

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,12 @@
+a = ['sibuyas', 'carrot', 'saluyot']
+
+if 'pineapple' in a:
+    print("You really like pineapple!")
+if 'apples' in a:
+    print("You really like apples!")
+if 'saluyot' in a:
+    print("You really like saluyot!")
+if 'talong' in a:
+    print("You really like talong!")
+if 'sibuyas' in a:
+    print("You really like sibuyas!")
---
+++ Chapter 4- Control Flow/Exercises/Exercise 8.py
@@ -0,0 +1,7 @@
+PW = (input("Enter your Password: "))
+PW2 =(input("Repeat your Password: "))
+
+if (PW == PW2):
+  print("Changed Password Complete")
+else:
+    print("Your password is incorrect, Please try again.")
